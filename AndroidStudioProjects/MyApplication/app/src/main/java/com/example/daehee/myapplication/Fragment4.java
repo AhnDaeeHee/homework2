87e4a87b98316a058bbd8e928e0706e1a945b2c1
AhnDaeHee <ahn8609@gmail.com>
2018-04-08 15:44:33 +0900

    add .

---
+++ AndroidStudioProjects/MyApplication/app/src/main/java/com/example/daehee/myapplication/Fragment4.java
@@ -0,0 +1,32 @@
+package com.example.daehee.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2016-10-11.
+ */
+public class Fragment4 extends Fragment {
+
+    public static Fragment4 newInstance(){
+        Fragment4 fragment = new Fragment4();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment4,container,false);
+
+        return view;
+    }
+}
